87f1e8d850791b639f0f21155aef273fa8866006
Willy <wcurtiscollins@gmail.com>
2012-03-27 13:01:18 -0400

    damn you conflicts

---
+++ Semester_5/Real-Time/Assignments/Assignment2/W12 CST 8244 Assignment 2/arith.h
@@ -0,0 +1,17 @@
+/* arith.h
+ * Algonquin College
+ * CST 8244
+ * Assignment 2
+ * Winter 2012
+*/
+
+typedef struct {
+    double operand1;
+    double operand2;
+    double sum;
+    double difference;
+    double product;
+    double quotient;
+} arith;
+
+/* eof: arith.h */
